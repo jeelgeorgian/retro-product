1244a373f353bee2ce8ff15b10ad1ff7a4fc454f
jeelgeorgian <141691964+jeelgeorgian@users.noreply.github.com>
2023-08-08 06:37:56 +0530

    Project Files Upload

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// JavaScript code
+
+// JQuery Code to load the header file
+$('header').load('/src/includes/header.html');
+// JQuery Code to load the footer file
+$('footer').load('/src/includes/footer.html');
