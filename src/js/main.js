c5436db8bb8724d68c4840982893bbf0c1504923
jeelgeorgian <141691964+jeelgeorgian@users.noreply.github.com>
2023-08-08 06:53:49 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // JavaScript code
 
 // JQuery Code to load the header file
-$('header').load('/src/includes/header.html');
+$('header').load('header.html');
 // JQuery Code to load the footer file
-$('footer').load('/src/includes/footer.html');
+$('footer').load('footer.html');
